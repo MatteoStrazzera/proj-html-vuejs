fd300cc03a604aac4217de8673c62952f0b614f3
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-08 16:18:04 +0200

    add components header,main,footer and import in app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
 
+export default{
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return{
+
+    }
+  }
+}
 </script>
 
 <template>
-  
+
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+
 </template>
 
 <style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: 'AppFooter',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+  <h1>Footer</h1>
+</template>
+
+<style>
+
+</style>
